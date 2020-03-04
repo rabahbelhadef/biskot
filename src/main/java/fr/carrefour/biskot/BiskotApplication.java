6162f0031f9756e0b8d039a95ea332efde786edf
Rabah Belhadef <ext-rabah.belhadef@tessi.fr>
2020-03-04 23:14:25 +0100

    initial commit

---
+++ src/main/java/fr/carrefour/biskot/BiskotApplication.java
@@ -0,0 +1,13 @@
+package fr.carrefour.biskot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiskotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiskotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/carrefour/biskot/BiskotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.carrefour.biskot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiskotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
