c7f6f07be2b10ec718b523388d521afd2b54a569
Rabah Belhadef <ext-rabah.belhadef@tessi.fr>
2020-03-07 22:26:15 +0100

    Add Rest Excetion Hundler

---
+++ src/main/java/fr/carrefour/biskot/rest/config/RestExceptionHundler.java
@@ -0,0 +1,21 @@
+package fr.carrefour.biskot.rest.config;
+
+import fr.carrefour.biskot.exception.BusinessException;
+import fr.carrefour.biskot.exception.DataNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHundler {
+
+    @ExceptionHandler(value = DataNotFoundException.class)
+    public ResponseEntity<Object> dataNotFound(DataNotFoundException exception) {
+        return ResponseEntity.notFound().header("error_message" , exception.getMessage()).build() ;
+    }
+
+    @ExceptionHandler(value = BusinessException.class)
+    public ResponseEntity<String> businessException(BusinessException exception) {
+        return ResponseEntity.badRequest().header("error_message" , exception.getMessage()).build() ;
+    }
+}
